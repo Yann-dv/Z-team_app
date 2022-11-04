bedf739f25359345d53495b2c59232a11398b588
Yann Hagege <yhagege@d-edge.com>
2022-11-04 15:02:17 +0100

    about template added

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,27 +5,27 @@ function Gallery() {
         <h2 className="gallery-title">Gallery</h2>
         <div className="gallery-flex">
           <div className="galleryCard" id="galleryCard_1">
-            <img className="galleryImage img1" src={require("../img/image.png")} alt="Gallery image"></img>
+            <img className="galleryImage img1" src={require("../img/image.png")} alt="Gallery"></img>
             <div className="galleryDescription">Gallery description to complete</div>
           </div>
           <div className="galleryCard" id="galleryCard_2">
-            <img className="galleryImage img2" src={require("../img/image.png")} alt="Gallery image"></img>
+            <img className="galleryImage img2" src={require("../img/image.png")} alt="Gallery"></img>
             <div className="galleryDescription">Gallery description to complete</div>
           </div>
           <div className="galleryCard" id="galleryCard_3">
-            <img className="galleryImage img3" src={require("../img/image.png")} alt="Gallery image"></img>
+            <img className="galleryImage img3" src={require("../img/image.png")} alt="Gallery"></img>
             <div className="galleryDescription">Gallery description to complete</div>
           </div>
           <div className="galleryCard" id="galleryCard_4">
-            <img className="galleryImage img4" src={require("../img/image.png")} alt="Gallery image"></img>
+            <img className="galleryImage img4" src={require("../img/image.png")} alt="Gallery"></img>
             <div className="galleryDescription">Gallery description to complete</div>
           </div>
           <div className="galleryCard" id="galleryCard_5">
-            <img className="galleryImage img5" src={require("../img/image.png")} alt="Gallery image"></img>
+            <img className="galleryImage img5" src={require("../img/image.png")} alt="Gallery"></img>
             <div className="galleryDescription">Gallery description to complete</div>
           </div>
           <div className="galleryCard" id="galleryCard_6">
-            <img className="galleryImage img6"  src={require("../img/image.png")} alt="Gallery image"></img>
+            <img className="galleryImage img6" src={require("../img/image.png")} alt="Gallery"></img>
             <div className="galleryDescription">Gallery description to complete</div>
           </div>
         </div>
