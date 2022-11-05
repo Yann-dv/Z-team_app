751247a70af42f6d5649b29129aafbe32d57b018
Yann Dev <h.yann_pro@yahoo.fr>
2022-11-05 14:34:50 +0100

    schedule-flex added

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ function HomeContent() {
           a large number of high level teachers, national and international
           competitors, as well as a large number of students. The disciplines of
           Brazilian Jiu-jitsu and Grappling have been known for several decades,
-          in particular through MMA (Mixed Martial Arts) from the 90's. These
+          in particular through MMA (Mixed Martial Arts) from the 90's. <br/><br/>These
           martial arts include phases of standing wrestling, but are mainly
           practiced on the ground. Brazilian Jiu-Jitsu is often compared to the
           game of chess, requiring as much intellectual, physical and strategic
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -2,41 +2,54 @@ function Schedule() {
   return (
     <section className="scheduleContainer">
       <div className="scheduleBackground">
-      <h2 className="schedule-title">Schedule</h2>
-        <table className="bjj schedule">
-          <tbody>
-            <tr>
-              <th scope="colgoup" rowSpan="1" colSpan="2">BJJ Schedule</th>
-            </tr>
+        <h2 className="schedule-title">Schedule</h2>
+        <div className="schedule-flex">
+          <table className="bjj schedule">
+            <tbody>
+              <tr>
+                <th scope="colgoup" rowSpan="1" colSpan="2">
+                  BJJ Schedule
+                </th>
+              </tr>
 
-            <tr>
-              <td>Tuesday, Thursday</td>
-              <td>20:00 - 22:00</td>
-            </tr>
+              <tr>
+                <td>Tuesday, Thursday</td>
+                <td>20:00 - 22:00</td>
+              </tr>
 
-            <tr>
-              <td>Wednesday</td>
-              <td>21:00 - 22:30</td>
-            </tr>
+              <tr>
+                <td>Wednesday</td>
+                <td>21:00 - 22:30</td>
+              </tr>
 
-            <tr>
-              <td>Saturday</td>
-              <td>12:00 - 14:00 <br/>18:30 - 20:30</td>
-            </tr>
-          </tbody>
-        </table>
+              <tr>
+                <td>Saturday</td>
+                <td>
+                  12:00 - 14:00 <br />
+                  18:30 - 20:30
+                </td>
+              </tr>
+            </tbody>
+          </table>
 
-        <table className="grappling schedule">
-          <tbody>
-            <tr>
-              <th scope="colgoup" rowSpan="1" colSpan="2">Grappling Schedule</th>
-            </tr>
-            <tr>
-              <td>Wednesday</td>
-              <td>21:00 - 22:30</td>
-            </tr>
-          </tbody>
-        </table>
+          <table className="grappling schedule">
+            <tbody>
+              <tr>
+                <th scope="colgoup" rowSpan="1" colSpan="2">
+                  Grappling Schedule
+                </th>
+              </tr>
+              <tr>
+                <td>Wednesday</td>
+                <td>21:00 - 22:30</td>
+              </tr>
+              <tr>
+                <td>Saturday</td>
+                <td>18:30 - 20:30</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </section>
   );
