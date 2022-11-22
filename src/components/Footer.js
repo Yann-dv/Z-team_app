6b906582832351d30c492f60b40efb5800eed165
Yann HAGEGE <yhagege@ds.d-edge.com>
2022-11-22 12:53:53 +0100

    z-team_app path added and csp

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ function Footer() {
         <footer className="footerContainer">
             <ul className="footerList">
                 <li className="footerLi"><a className="footerLink" href="mailto:zteam.boulogne@gmail.com">Contact</a></li>
-                <li className="footerLi"><Link className="footerLink" to="/legal">Legal Notice</Link></li>
-                <li className="footerLi"><Link className="footerLink" to="/">Terms of Service</Link></li>
-                <li className="footerLi"><Link className="footerLink" to="/privacy">Privacy Policy</Link></li>
+                <li className="footerLi"><Link className="footerLink" to="/z-team_app/legal">Legal Notice</Link></li>
+                <li className="footerLi"><Link className="footerLink" to="/z-team_app/">Terms of Service</Link></li>
+                <li className="footerLi"><Link className="footerLink" to="/z-team_app/privacy">Privacy Policy</Link></li>
             </ul>
         </footer>
         );
