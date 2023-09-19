1fb58465682878e6edb66eaabd92acba426716eb
Yann <79877110+Yann-dv@users.noreply.github.com>
2023-09-19 21:06:33 +0200

    chore: update routes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
         <footer className="footerContainer">
             <ul className="footerList">
                 <li className="footerLi"><a className="footerLink" href="mailto:zteam.boulogne@gmail.com">Contact</a></li>
-                <li className="footerLi"><Link className="footerLink" to="/z-team_app/legal">Mentions légales</Link></li>
-                <li className="footerLi"><Link className="footerLink" to="/z-team_app/">Conditions d'utilisation</Link></li>
-                <li className="footerLi"><Link className="footerLink" to="/z-team_app/privacy">Politique de confidentialité</Link></li>
+                <li className="footerLi"><Link className="footerLink" to="/legal">Mentions légales</Link></li>
+                <li className="footerLi"><Link className="footerLink" to="/">Conditions d'utilisation</Link></li>
+                <li className="footerLi"><Link className="footerLink" to="/privacy">Politique de confidentialité</Link></li>
             </ul>
         </footer>
         );
